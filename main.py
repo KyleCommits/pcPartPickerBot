7a96a1c8cff6fe00bc6091a84f1236d705080ecc
At0micSurprise <core517@hotmail.com>
2023-05-22 13:29:34 -0400

    moar stuff

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-# This is a sample Python script.
+from pcpartpicker import API as pcpartpickerAPI
+from pcpartscraper.scraper import Part, Query
+from amazon_buddy import Product, AmazonBuddy, Category, SortType
+import pprint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def part_picker_main():
+    # method 1:
+    part_picker_api = pcpartpickerAPI('us')
+    # print(part_picker_api.supported_parts)
+    cpu_data = part_picker_api.retrieve("cpu")
+    pprint.pprint(len(cpu_data.get('cpu', [])))
+    # print(cpu_data.get('cpu')[200])
+    cpu = cpu_data.get('cpu')[200]
+    pprint.pprint(dir(cpu))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # amazon
+    # ab = AmazonBuddy(debug=True, user_agent='ADD_USER_AGENT')
+    # products = ab.search_products(
+    #     'ryzen cpu',
+    #     sort_type=SortType.PRICE_HIGH_TO_LOW,
+    #     min_price=0,
+    #     category=Category.BEAUTY_AND_PERSONAL_CARE,
+    #     max_results=20
+    # )
+    # print(len(products))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# test
+    part_picker_main()
